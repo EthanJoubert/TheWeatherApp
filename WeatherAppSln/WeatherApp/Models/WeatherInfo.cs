ace31b6b80d9c2612a970e039bdcd62e70babcf2
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-04-04 14:05:38 +0200

    Weather class created

---
+++ WeatherAppSln/WeatherApp/Models/WeatherInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    class WeatherInfo
+    {
+        public int ID { get; set; }
+        public string Temperture { get; set; }
+        public string Humidity { get; set; }
+        public string WindSpeeds { get; set; }
+    }
+}
--- WeatherAppSln/WeatherApp/Pages/WeatherPage.xaml.cs
+++ WeatherAppSln/WeatherApp/Pages/WeatherPage.xaml.cs
@@ -1,9 +1,39 @@
+using Newtonsoft.Json;
+using WeatherApp.Models;
+
 namespace WeatherApp.Pages;
 
-public partial class NewPage1 : ContentPage
+public partial class WeatherPage : ContentPage
 {
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
+    private string _test;
+
+    public string Test
+    {
+        get { return _test; }
+        set { _test = value; }
+    }
+
+    private HttpClient _httpClient;
+    public WeatherPage()
+    {
+        InitializeComponent();
+        _httpClient = new HttpClient();
+        // Request Header
+        _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+    }
+
+    public async void GetLatestJoke(object parameters)
+    {
+        // Gets the Dad Joke Api and its data and saves it in "response"
+        string response = await _httpClient.GetStringAsync(new Uri("https://icanhazdadjoke.com"));
+
+        // Turning the response from JSON to C#
+        WeatherInfo latestjoke = JsonConvert.DeserializeObject<WeatherInfo>(response);
+
+        // Getting the joke out the class and displaying it
+        if (latestjoke != null)
+        {
+            Test = latestjoke.Temperture;
+        }
+    }
 }
