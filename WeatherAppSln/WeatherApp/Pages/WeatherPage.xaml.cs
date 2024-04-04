fe1e900592646ec94d58587e484d4d50caebd835
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-04-04 15:28:00 +0200

    json to C# class created

--- WeatherAppSln/WeatherApp/Pages/WeatherPage.xaml.cs
+++ WeatherAppSln/WeatherApp/Pages/WeatherPage.xaml.cs
@@ -5,9 +5,9 @@ namespace WeatherApp.Pages;
 
 public partial class WeatherPage : ContentPage
 {
-    private string _test;
-
-    public string Test
+    // Properties
+    private double _test;
+    public double Test
     {
         get { return _test; }
         set { _test = value; }
@@ -26,15 +26,15 @@ public partial class WeatherPage : ContentPage
     public async void GetLatestJoke(object parameters)
     {
         // Gets the Weather Api and its data and saves it in "response"
-        string response = await _httpClient.GetStringAsync(new Uri("https://icanhazdadjoke.com"));
+        string response = await _httpClient.GetStringAsync(new Uri("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=22d84222850961e3fe8cc19c603cab33"));
 
         // Turning the response from JSON to C#
-        WeatherInfo info = JsonConvert.DeserializeObject<WeatherInfo>(response);
-
+        WeatherInformtaion info = JsonConvert.DeserializeObject<WeatherInformtaion>(response);
+         
         // Getting the info out the class and displaying it
         if (info != null)
         {
-            Test = info.Temperture;
+            Test = info.main.temp;
         }
     }
 }
